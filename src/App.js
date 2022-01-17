2c933e1423c1ec18069f6fdc793ef4ab2b6fba0c
Andre Pires <brainshutdown@gmail.com>
2022-01-17 22:50:26 +0000

    JSX added to components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import SetupForm from './SetupForm';
 import Loading from './Loading';
 import Modal from './Modal';
 function App() {
-	return <h2>quiz starter</h2>;
+	const { loading } = useGlobalContext();
+
+	if (loading) return <Loading />;
+
+	return (
+		<main>
+			{/* TODO: Show modal only after all questions have been presented */}
+			<Modal />
+			<SetupForm />
+		</main>
+	);
 }
 
 export default App;
--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { useGlobalContext } from './context';
 
 const Modal = () => {
-	return <h2>modal component</h2>;
+	return (
+		<div className='modal-container'>
+			<div className='modal-content'>
+				{/* TODO: Change message depending on correct answers */}
+				{/* TODO: Change percentage of correct answers */}
+				<h2>Congratulations!</h2>
+				<p>You answered 0% of questions correctly</p>
+				<button className='close-btn'>Play Again</button>
+			</div>
+		</div>
+	);
 };
 
 export default Modal;
